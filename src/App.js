57019fb7f768f1fa13b5773ba4cfda96e3cf8ad0
Rayhan0Islam0Shagor <raihanislamshagor2@gmail.com>
2022-11-02 20:03:53 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,44 +3,50 @@ import Container from "@mui/material/Container";
 import "./App.css";
 import JoditEditor from "jodit-react";
 import { Box } from "@mui/material";
+import RichTextEditorJodit from "./Components/RichTextEditorJodit";
 
 function App() {
-  const editor = React.useRef(null);
-  const [content, setContent] = React.useState("");
+  const [value, setValue] = React.useState("");
+  const getValue = (value) => {
+    setValue(value);
+  };
 
-  const editorConfig = React.useMemo(
-    () => ({
-      readonly: false,
-      toolbarSticky: false,
-      toolbarButtonSize: "small",
-      toolbar: true,
-      buttons: [
-        "bold",
-        "italic",
-        "underline",
-        "strikethrough",
-        "|",
-        "ul",
-        "ol",
-        "|",
-        "outdent",
-        "indent",
-        "|",
-        "font",
-        "fontsize",
-        "brush",
-        "paragraph",
-        "|",
-        "align",
-        "undo",
-        "redo",
-      ],
-      uploader: {
-        url: "https://xdsoft.net/jodit/connector/index.php?action=fileUpload",
-      },
-    }),
-    []
-  );
+  // const editor = React.useRef(null);
+  // const [content, setContent] = React.useState("");
+
+  // const editorConfig = React.useMemo(
+  //   () => ({
+  //     readonly: false,
+  //     toolbarSticky: false,
+  //     toolbarButtonSize: "small",
+  //     toolbar: true,
+  //     buttons: [
+  //       "bold",
+  //       "italic",
+  //       "underline",
+  //       "strikethrough",
+  //       "|",
+  //       "ul",
+  //       "ol",
+  //       "|",
+  //       "outdent",
+  //       "indent",
+  //       "|",
+  //       "font",
+  //       "fontsize",
+  //       "brush",
+  //       "paragraph",
+  //       "|",
+  //       "align",
+  //       "undo",
+  //       "redo",
+  //     ],
+  //     uploader: {
+  //       url: "https://xdsoft.net/jodit/connector/index.php?action=fileUpload",
+  //     },
+  //   }),
+  //   []
+  // );
 
   return (
     <Box
@@ -49,16 +55,11 @@ function App() {
       }}
     >
       <Container>
-        <JoditEditor
-          ref={editor}
-          value={content}
-          onChange={(newContent) => setContent(newContent)}
-          config={editorConfig}
-        />
+        <RichTextEditorJodit initialValue="" getValue={getValue} />
 
         <Box sx={{ p: 2, bgcolor: "grey.300" }}>
           <h1>Preview</h1>
-          <div dangerouslySetInnerHTML={{ __html: content }} />
+          <div dangerouslySetInnerHTML={{ __html: value }} />
         </Box>
       </Container>
     </Box>
