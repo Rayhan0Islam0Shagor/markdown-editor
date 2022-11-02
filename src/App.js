83394d34aea61c92c9f1d5358b3c50db06b99947
Rayhan0Islam0Shagor <raihanislamshagor2@gmail.com>
2022-11-02 19:32:24 +0600

    refactor buttons

--- src/App.js
+++ src/App.js
@@ -7,19 +7,93 @@ import { Box } from "@mui/material";
 function App() {
   const editor = React.useRef(null);
   const [content, setContent] = React.useState("");
-  console.log("🚀 ~ file: App.js ~ line 8 ~ App ~ content", content);
+
+  const config = {
+    readonly: false,
+    toolbarButtonSize: "small",
+    toolbarSticky: false,
+    toolbar: true,
+    buttons: [
+      "bold",
+      "italic",
+      "underline",
+      "strikethrough",
+      "|",
+      "ul",
+      "ol",
+      "|",
+      "outdent",
+      "indent",
+      "|",
+      "font",
+      "fontsize",
+      "brush",
+      "paragraph",
+      "|",
+      "align",
+      "undo",
+      "redo",
+    ],
+  };
 
   return (
-    <Container>
-      <JoditEditor
-        ref={editor}
-        value={content}
-        onChange={(newContent) => setContent(newContent)}
-      />
+    <Box
+      sx={{
+        padding: "2rem 0",
+      }}
+    >
+      <Container>
+        <JoditEditor
+          ref={editor}
+          value={content}
+          onChange={(newContent) => setContent(newContent)}
+          config={config}
+        />
 
-      <Box dangerouslySetInnerHTML={{ __html: content }} />
-    </Container>
+        <Box sx={{ p: 2, bgcolor: "grey.300" }}>
+          <h1>Preview</h1>
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
 export default App;
+
+// buttons: [
+//   "bold",
+//   "italic",
+//   "underline",
+//   "strikethrough",
+//   "|",
+//   "ul",
+//   "ol",
+//   "|",
+//   "outdent",
+//   "indent",
+//   "|",
+//   "font",
+//   "fontsize",
+//   "brush",
+//   "paragraph",
+//   "|",
+//   "image",
+//   "file",
+//   "video",
+//   "table",
+//   "link",
+//   "|",
+//   "align",
+//   "undo",
+//   "redo",
+//   "hr",
+//   "eraser",
+//   "copyformat",
+//   "symbol",
+//   "fullsize",
+//   "print",
+//   "cut",
+//   "selectall",
+// ],
+// }}
